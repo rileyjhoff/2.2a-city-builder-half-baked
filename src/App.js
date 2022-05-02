9566ea79c95ab8aaadeec6cba0e7ea934ecb8ff2
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-02 11:59:32 -0700

    add components to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import './App.css';
-import SkylineDropdown from './SkylineDropdown';
-import WaterfrontDropdown from './WaterfrontDropdown';
-import CastleDropdown from './CastleDropdown';
-import SloganList from './SloganList';
-import SloganForm from './SloganForm';
-import CityNameInput from './CityNameInput';
-import City from './City';
+import SkylineDropdown from './components/SkylineDropdown';
+import WaterfrontDropdown from './components/WaterfrontDropdown';
+import CastleDropdown from './components/CastleDropdown';
+import SloganList from './components/SloganList';
+import SloganForm from './components/SloganForm';
+import CityNameInput from './components/CityNameInput';
+import City from './components/City';
 
 function App() {
   // track some state here.
@@ -17,21 +17,28 @@ function App() {
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City />
       <h1>
         {/* dynamically update the city name here using state */}
         Welcome to beautiful Portland!
       </h1>
       <div className="bottom">
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
+        <CityNameInput />
         <section className="dropdowns">
           {/* 
           render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
           
           In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
           */}
+          <WaterfrontDropdown />
+          <SkylineDropdown />
+          <CastleDropdown />
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
+        <SloganForm />
+        <SloganList />
       </div>
     </div>
   );
