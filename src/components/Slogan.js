91820b0671437ad9ed7cd2ad398e3c470ef79f9f
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-02 12:42:32 -0700

    Slogan now returns a div with a p tag, which contains the slogan text

--- src/components/Slogan.js
+++ src/components/Slogan.js
@@ -4,7 +4,7 @@ export default function Slogan({ slogan }) {
   return (
     <div className="slogan">
       {/* this component takes in a slogan as a prop. Render it out here however you like */}
-      {slogan}
+      <p>{slogan}</p>
     </div>
   );
 }
